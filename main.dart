68be08d7f9f1a42ec205daba94d1ecbaacfd2de0
satyanarayan meena <40121146+SatyaNM95@users.noreply.github.com>
2020-07-29 13:27:28 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+import 'package:layout_app/ui/home.dart';
+main() {
+  runApp(Myapp());
+
+  
+}
+class Myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return testapp();
+  }
+}
